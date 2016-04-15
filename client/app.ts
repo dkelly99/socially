69506d202a8b25d414b94b2849c784a6ef761a10
Declan Kelly <declan.kelly@btinternet.com>
2016-04-15 20:41:27 +0100

    step 4

--- client/app.ts
+++ client/app.ts
@@ -4,10 +4,12 @@ import {Mongo} from 'meteor/mongo';
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Parties} from '../collections/parties';
+import {PartiesForm} from '../client/imports/parties-form/parties-form';
 
 @Component({
     selector: 'app',
-    templateUrl: 'client/app.html'
+    templateUrl: 'client/app.html',
+    directives: [PartiesForm]
 })
 
 class Socially {
@@ -15,6 +17,10 @@ class Socially {
 
     constructor () {
         this.parties = Parties.find();
+    }
+
+    removeParty(party) {
+        Parties.remove(party._id);
     }
 }
 
